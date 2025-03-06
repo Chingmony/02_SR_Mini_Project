ae10cd6862a74e3c201e02c14e6f977a22e12c53
SikeatOeng <oeng.sikeat.1222@rupp.edu.kh>
2025-03-06 16:16:20 +0700

    delete comment

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-import org.w3c.dom.ls.LSOutput;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.println(" W) Write       R) Read (id)       U) Update       D) Delete        S) Search (name)       Se) Set rows");
         System.out.println(" sa) Save       Un) Unsaved        Ba) Backup      Re) Restore      E) Exit");
         System.out.println(" -----------------------------------------------------------------------------");
